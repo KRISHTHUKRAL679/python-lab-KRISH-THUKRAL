0786335b5c889da54bcfe493f3c4bad9c8bc69ab
KRISHTHUKRAL679 <krish.23fe10cse00679@muj.manipal.edu>
2024-09-23 19:31:41 +0530

    Update  Xn.py

--- lab 3/ Xn.py
+++ lab 3/ Xn.py
@@ -3,8 +3,6 @@ def compute_power(base, exponent):
 try:
     base = float(input("Enter the base (X): "))
     exponent = int(input("Enter the exponent (n): "))
-    
     result = compute_power(base, exponent)
-    
     print(f"{base} raised to the power of {exponent} is {result}")
 
