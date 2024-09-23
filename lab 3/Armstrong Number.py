eab76555e88e936fe82a155f21068febe675580e
KRISHTHUKRAL679 <krish.23fe10cse00679@muj.manipal.edu>
2024-09-23 19:28:20 +0530

    Update Armstrong Number.py

--- lab 3/Armstrong Number.py
+++ lab 3/Armstrong Number.py
@@ -1,11 +1,21 @@
-def is_armstrong_number(number):
-    str_num = str(number)
-    num_digits = len(str_num)
-    sum_of_powers = sum(int(digit) ** num_digits for digit in str_num)
-   
-    return sum_of_powers == number
+def length(num):
+    len = 0
+    while(num!=0):
+        len = len + 1
+        num //= 10
+    return len
 
-print("Armstrong Numbers from 1 to 1000:")
-for num in range(1, 1001):
-    if is_armstrong_number(num):
-        print(num)
+lower = 1
+upper = 1000
+
+for num in range(lower, upper + 1):
+    sum = 0
+    temp = num
+    len = length(num)
+
+    while(temp>0):
+        digit = temp%10
+        sum += digit ** len
+        temp //=10
+
+    if(num == sum): print(num)
